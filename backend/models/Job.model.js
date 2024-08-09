e936ef82c55562f9cce17dbb187bf762edb083ba
VinaySingh1045 <vy0891547@gmail.com>
2024-08-09 16:39:34 +0530

    Added Job model

---
+++ backend/models/Job.model.js
@@ -0,0 +1,57 @@
+import mongoose, { Types } from "mongoose";
+
+const JobSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        requirements: [{
+            type: String
+        }],
+        salary: {
+            type: Number,
+            required: true
+        },
+        experienceLevel: {
+            type: Number,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        jobType: {
+            type: String,
+            enum: ["Full-Time", "Part-Time"],
+            required: true
+        },
+        positionsAvailable: {
+            type: Number,
+            required: true
+        },
+        company: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Company",
+            required: true
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        application: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Application",
+        }
+
+    },
+    {
+        timestamps: true
+    })
+
+export const Job = mongoose.model("Job", JobSchema)
