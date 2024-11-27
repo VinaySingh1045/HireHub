29c68ea5c8a8ff28a0aad96451cc73a4ee7de6c2
VinaySingh1045 <vy0891547@gmail.com>
2024-11-27 05:40:23 +0530

    Deploy modification

--- frontend/src/utlis/constants.js
+++ frontend/src/utlis/constants.js
@@ -1,9 +1,15 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/users"
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job"
-export const APPLICATION_API_END_POINT = "http://localhost:3000/api/v1/application"
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company"
-export const ADMIN_API_END_POINT = "http://localhost:3000/api/v1/admin"
+// export const USER_API_END_POINT = "http://localhost:3000/api/v1/users"
+// export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job"
+// export const APPLICATION_API_END_POINT = "http://localhost:3000/api/v1/application"
+// export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company"
+// export const ADMIN_API_END_POINT = "http://localhost:3000/api/v1/admin"
 
+
+export const USER_API_END_POINT = "https://hirehub-1-hzj2.onrender.com/api/v1/users"
+export const JOB_API_END_POINT = "https://hirehub-1-hzj2.onrender.com/api/v1/job"
+export const APPLICATION_API_END_POINT = "https://hirehub-1-hzj2.onrender.com/api/v1/application"
+export const COMPANY_API_END_POINT = "https://hirehub-1-hzj2.onrender.com/api/v1/company"
+export const ADMIN_API_END_POINT = "https://hirehub-1-hzj2.onrender.com/api/v1/admin"
 
 // export const USER_API_END_POINT = "https://hirehub-two.vercel.app/api/v1/users"
 // export const JOB_API_END_POINT = "https://hirehub-two.vercel.app/api/v1/job"
